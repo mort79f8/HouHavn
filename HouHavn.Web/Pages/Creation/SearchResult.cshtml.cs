22b02ae03327c7a0f0196114b0cec53915bdd629
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-10-24 09:33:30 +0200

    Search page created and mostly function.

--- HouHavn.Web/Pages/Creation/SearchResult.cshtml.cs
+++ HouHavn.Web/Pages/Creation/SearchResult.cshtml.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HouHavn.Web.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouHavn.Web.Pages.Creation
 {
     public class SearchResultModel : PageModel
     {
+        private readonly HouhavnContext _context;
 
-        public void OnGet()
+        [BindProperty(SupportsGet = true)]
+        public string SearchQuery { get; set; }
+        public List<Boat> Boatlist { get; set; }
+
+
+        public IList<Berth> Berth { get; set; }
+        public IList<Boat> Boats { get; set; }
+        public IList<Person> People { get; set; }
+
+        public SearchResultModel(HouhavnContext context)
         {
+            _context = context;
 
+        }
+
+
+        public async Task OnGetAsync()
+        {
+            Berth = await _context.Berths.ToListAsync();
+            Boats = await _context.Boats.ToListAsync();
+            People = await _context.Persons.ToListAsync();
+            Search(SearchQuery);
+        }
+
+        private void Search(string searchquery)
+        {
+            Boatlist = Boats.Where(b =>
+                b.PersonNavigation.FullName.ToLower().Contains(searchquery.ToLower()) ||
+                b.Name.ToLower().Contains(searchquery.ToLower()) ||
+                b.Type.ToLower().Contains(searchquery.ToLower())
+                ).ToList();
         }
     }
 }
